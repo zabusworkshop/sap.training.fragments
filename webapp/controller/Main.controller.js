c62fb28adc31708c6859060de925178ad044ccbe
zabusworkshop <eugeny.zabus@gmail.com>
2019-12-23 06:17:38 +0000

    initial state

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("sap.training.controller.Main", {
+
+		onXmlButtonPress: function(oEvent) {
+		},
+
+		onJsButtonPress: function(oEvent) {
+		}
+
+	});
+});
